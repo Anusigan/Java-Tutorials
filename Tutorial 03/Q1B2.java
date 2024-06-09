a7832ff50cf95c854f5335c5293cd899476f8702
Anusigan <sivananthananusigan@gmail.com>
2024-06-10 02:00:32 +0530

    java tutorials

---
+++ Tutorial 03/Q1B2.java
@@ -0,0 +1,15 @@
+public class Q1B2{
+        public static void main(String[] args) {
+        int count=0;
+        while(count<=14){
+            if(count==6){
+                count+=2;
+                continue;
+            }
+            else{
+                System.out.println(count);
+                count+=2;
+            }
+        }      
+    }
+}
---
+++ Tutorial 03/Q7.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Q7 {
+    public static void main(String[] args) {
+        int chances=4;
+       String passcode="486251";
+        String user=" ";
+        Scanner sc=new Scanner(System.in);
+        for(int i=0;i<chances;i++){
+            System.out.println("Enter the passcode: ");
+            user=sc.nextLine();
+        
+            if(user.equals(passcode)){
+                System.out.println("Correct code!!");
+                break;
+            }
+            else{
+                System.out.println("Wrong passcode try again! You have "+(3-i)+" chances left");
+            }
+            sc.close();
+            
+
+        }
+    }
+
+}
