154b6bdcd149752b0f8ca2da270e1989a02a728b
Anusigan <sivananthananusigan@gmail.com>
2024-06-10 00:14:08 +0530

    changed

--- Tutorial 03/Q6.java
+++ Tutorial 03/Q6.java
@@ -1,19 +1,77 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 public class Q6 {
     public static void main(String[] args){
-    int num01=0;
-    int num02=0;
+    int num1=0;
+    int num2=0;
     char operator;
+    int result=0;
     Scanner sc=new Scanner(System.in);
+    while (true){
     System.out.println("Enter number 01: ");
     try{
-    num01=sc.nextInt();
-    };
-    except{}
-    sc.nextLine();
+    num1=sc.nextInt();
+    break;
+    }
+    catch(InputMismatchException e){
+        System.out.println("Enter a integer number!");
+        sc.next();
+    }
+}
+while(true){
+    System.out.println("Enter number 02: ");
+    try{
+    num2=sc.nextInt();
+    break;
+    }
+    catch(InputMismatchException e){
+        System.out.println("Enter a integer number!");
+        sc.next();
+    }
+}
+
+while (true){
+    System.out.println("Enter the operator: ");
+    operator=sc.next().charAt(0);
+    if(operator=='+'||operator=='-' || operator=='*' || operator=='/'){
+        break;
+    }
+    else{
+        System.out.println("Invalid operator");
+    }
+}
+switch(operator){
+    case '+':{
+        result=num1+num2;
+        break;
+    }
+
+    case ('-'):{
+        result=num1-num2;
+        break;
+    }
+
+    case('*'):{
+        result=num1*num2;
+        break;
+    }
+
+    case('/'):{
+        result=num1/num2;
+        break;
+    }
+
+    }
+    System.out.println("The result is: "+result);
+    sc.close();
+}
+}
+
+
+    
+    
+
 
     
 
-    
-}
-}
+
---
+++ Tutorial 03/Q8.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.InputMismatchException;
+public class Q8 {
+    public static void main(String[] args) {
+        Random rand=new Random();
+        Scanner sc=new Scanner(System.in);
+        int number=rand.nextInt(1,20);
+        int choice=0;
+        while (true){
+            System.out.println("Enter a choice: ");
+            try{
+                 choice=sc.nextInt();
+                 if(number==choice){
+                System.out.println("Your guess is correct!!");
+                break;
+                
+            }
+            else{
+                System.out.println("Try again !");
+            }
+        }
+            catch(InputMismatchException e){
+                System.out.println("Enter a valid integer digit");
+                sc.nextLine();
+            }
+        }
+        
+        sc.close();
+    }
+}
+        
+    
+
+
+
