72ca33aeed7adf728a147eb00b8be1f173e50be4
Anusigan Sivananthan <148673260+Anusigan@users.noreply.github.com>
2024-06-10 00:50:26 +0530

    Delete Tutorial 03 directory

--- Tutorial 03/Q1A2.java
+++
@@ -1,9 +0,0 @@
-public class Q1A2{
-    public static void main(String[] args){
-        int count=0;
-        while(count<=5){
-            System.out.println(count);
-            count++;
-        }
-    }
-}
